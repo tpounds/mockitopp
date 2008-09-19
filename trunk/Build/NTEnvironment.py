e927e1031cde84fefba05a92085d2528cf2ef5ef
trevor.pounds <trevor.pounds@1c9ccd7b-6d53-0410-aa7f-53e9b9f2c3a3>
2008-09-19 23:08:23 +0000

    fix support for setting CXX from environment add minimal support (i.e. still broken) for Microsoft Visual C++ compilers improve usage of svn:externals for boost libs (svn up speed improvement, smaller on disk)

--- trunk/Build/NTEnvironment.py
+++ trunk/Build/NTEnvironment.py
@@ -1,5 +1,42 @@
 import BaseEnvironment
+import re
+from subprocess import Popen, PIPE, STDOUT
 
 class Environment(BaseEnvironment.Environment):
    def __init__(self, **kw):
       apply(BaseEnvironment.Environment.__init__, (self,), kw)
+      self.DetectEnvironment()
+
+   def DetectEnvironment(self):
+      # TODO: check Microsoft Platform SDK
+
+      proc = Popen(self['CC'], stderr=STDOUT, stdout=PIPE)
+      proc.wait()
+      version = "".join(proc.stderr.readlines())
+
+      # configure compiler version
+      if re.compile("Version 13\.10").search(version):
+         self.Configure_MSVC_7_1()
+      elif re.compile("Version 14\.00").search(version):
+         self.Configure_MSVC_8_0()
+      elif re.compile("Version 15\.00").search(version):
+         self.Configure_MSVC_9_0()
+      # TODO: MINGW
+      # TODO: raise error otherwise
+
+      if re.compile("Microsoft").search(version):
+         self.AppendUnique(CCFLAGS = "/EHsc")
+
+   # Microsoft Visual C++ .NET 2003 (7.1)
+   def Configure_MSVC_7_1(self):
+      binpath = os.path.dirname(self['CXX'])
+      self['LINK'] = binpath + "\link.exe"
+      print "MSVC 7.1 Detected"
+
+   # Microsoft Visual C++ 2005 (8.0)
+   def Configure_MSVC_8_0(self):
+      print "MSVC 8.0 Detected"
+
+   # Microsoft Visual C++ 2008 (9.0)
+   def Configure_MSVC_9_0(self):
+      print "MSVC 9.0 Detected"
