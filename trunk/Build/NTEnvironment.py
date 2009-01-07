1726fb6026da2fb331ba085ff7a00966d492e0a5
trevor.pounds <trevor.pounds@1c9ccd7b-6d53-0410-aa7f-53e9b9f2c3a3>
2009-01-07 23:44:50 +0000

    better environment support for MSVC++ and MinGW on Windows

--- trunk/Build/NTEnvironment.py
+++ trunk/Build/NTEnvironment.py
@@ -6,36 +6,70 @@ from subprocess import Popen, PIPE, STDOUT
 class Environment(BaseEnvironment.Environment):
    def __init__(self, **kw):
       apply(BaseEnvironment.Environment.__init__, (self,), kw)
-      self.DetectEnvironment()
 
-   def DetectEnvironment(self):
-      # TODO: check Microsoft Platform SDK
+      self.ConfigureCompiler()
 
-      proc = Popen(self['CXX'], stderr=STDOUT, stdout=PIPE)
-      proc.wait()
-      version = "".join(proc.stdout.readlines())
+      self['ENV']['INCLUDE'] = self['CPPPATH']
+      self['ENV']['LIB'] = self['LIBPATH']
+      self['ENV']['PATH'] = os.environ['PATH']
 
-      # configure compiler version
-      if re.compile("Version 13\.10").search(version):
-         self.Configure_MSVC_7_1()
-      elif re.compile("Version 14\.00").search(version):
-         self.Configure_MSVC_8_0()
-      elif re.compile("Version 15\.00").search(version):
-         self.Configure_MSVC_9_0()
-      # TODO: MINGW
-      # TODO: raise error otherwise
+   def ConfigureCompiler(self):
+      cxx = self.WhereIs(self['CXX'])
+      cxx_bin = os.path.basename(cxx).lower()
+      cxx_dir = os.path.dirname(cxx)
 
-      if re.compile("Microsoft").search(version):
-         self.AppendUnique(CCFLAGS = "/EHsc")
+      if cxx_bin == "cl.exe":
+         common = cxx_dir + "\..\.."
+         if os.path.exists(common + "\Common\IDE"):
+           os.environ['PATH'] = common + "\Common\IDE" + ";" + cxx_dir
+         elif os.path.exists(common + "\Common7\IDE"):
+           os.environ['PATH'] = common + "\Common7\IDE" + ";" + cxx_dir
 
-   # Microsoft Visual C++ .NET 2003 (7.1)
-   def Configure_MSVC_7_1(self):
-      print "MSVC 7.1 Detected"
+         proc = Popen(cxx_bin, stderr=STDOUT, stdout=PIPE)
+         proc.wait()
+         version = "".join(proc.stdout.readlines())
 
-   # Microsoft Visual C++ 2005 (8.0)
-   def Configure_MSVC_8_0(self):
-      print "MSVC 8.0 Detected"
+         if re.search("Version 13\.10", version):
+            print "Microsoft Visual C++ .NET 2003 (7.1) Detected"
+         elif re.search("Version 14\.00", version):
+            print "Microsoft Visual C++ 2005 (8.0) Detected"
+         elif re.search("Version 15\.00", version):
+            print "Microsoft Visual C++ 2008 (9.0) Detected"
+         # TODO: handle unsupported msvc++ version
 
-   # Microsoft Visual C++ 2008 (9.0)
-   def Configure_MSVC_9_0(self):
-      print "MSVC 9.0 Detected"
+         self.AppendUnique(CCFLAGS = ['/EHsc'])
+         self['CXX'] = cxx_bin
+#         self['LINK'] = "link.exe"
+         self['CPPPATH'] = os.path.abspath(cxx_dir + "\..\include")
+         self['LIBPATH'] = os.path.abspath(cxx_dir + "\..\lib")
+
+         sdk_dir = ""
+         if os.getenv('MicrosoftPlatformSDK', None) != None:
+            sdk_dir = os.environ['MicrosoftPlatformSDK']
+         # standard MSVC++ relative install
+         elif os.path.exists(cxx_dir + "\..\PlatformSDK"):
+            sdk_dir = cxx_dir + "\..\PlatformSDK"
+         #TODO: handle directory not found
+
+         if os.path.exists(sdk_dir + "\include\windows.h"):
+            self.AppendUnique(CPPPATH=[sdk_dir + "\include"])
+            self.AppendUnique(LIBPATH=[sdk_dir + "\lib"])
+         #TODO: handle windows.h not found
+
+      elif cxx_bin == "g++.exe":
+         os.environ['PATH'] = cxx_dir
+         proc = Popen(cxx_bin + " --version", stderr=STDOUT, stdout=PIPE)
+         proc.wait()
+         version = "".join(proc.stdout.readlines())
+
+         match = re.search("\(GCC\) (\d.\d.\d)", version)
+         if match:
+            print "MinGW " + match.group(1) + " Detected"
+         # TODO: handle unsupported mingw version
+
+         self.Tool('mingw')
+         if '/nologo' in self['CCFLAGS']:
+            self['CCFLAGS'].remove('/nologo')
+
+         self.AppendUnique(CPPPATH=[os.path.abspath(cxx_dir + "\..\include")])
+         self.AppendUnique(LIBPATH=[os.path.abspath(cxx_dir + "\..\lib")])
