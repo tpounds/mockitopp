2fa2e4eab439d6a8014dde737eaee9ca0f076149
Trevor Pounds <trevor.pounds@gmail.com>
2009-02-16 04:53:14 +0000

    checkin missing argument matcher source file

---
+++ include/mockitopp/matchers/Matcher.hpp
@@ -0,0 +1,15 @@
+#ifndef __MOCKITOPP_MATCHER_HPP__
+#define __MOCKITOPP_MATCHER_HPP__
+
+namespace mockitopp
+{
+   template <typename T>
+   struct Matcher
+   {
+      virtual Matcher* clone() const = 0;
+
+      virtual bool operator== (const T& rhs) const = 0;
+   };
+} // namespace mockitopp
+
+#endif //__MOCKITOPP_MATCHER_HPP__
