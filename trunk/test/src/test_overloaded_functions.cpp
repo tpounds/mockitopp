6e344010cbcc07dbc12094ffd905469080e3d6af
trevor.pounds <trevor.pounds@1c9ccd7b-6d53-0410-aa7f-53e9b9f2c3a3>
2009-08-18 18:39:09 +0000

    add multiple inheritance test (not supported due to multiple vtables) fix implicit char* to boolean conversion issue in overloaded function test

--- trunk/test/src/test_overloaded_functions.cpp
+++ trunk/test/src/test_overloaded_functions.cpp
@@ -3,14 +3,12 @@
 
 using mockitopp::MockObject;
 
-// TODO: fix std::string impl
-
 struct overloaded_interface
 {
    virtual int foo() = 0;
    virtual int foo(bool) = 0;
    virtual int foo(char) = 0;
-//   virtual int foo(const std::string&) = 0;
+   virtual int foo(const std::string&) = 0;
 };
 
 TEST(test_overloaded_functions, foo)
@@ -20,8 +18,8 @@ TEST(test_overloaded_functions, foo)
    mock(static_cast<int (overloaded_interface::*)(bool)>(&overloaded_interface::foo)).when(true).thenReturn(0);
    mock(static_cast<int (overloaded_interface::*)(char)>(&overloaded_interface::foo)).when('A').thenReturn(0);
    mock(static_cast<int (overloaded_interface::*)(char)>(&overloaded_interface::foo)).when('%').thenReturn(1);
-//   mock(static_cast<int (overloaded_interface::*)(const std::string&)>(&overloaded_interface::foo)).when("bar").thenReturn(50);
-//   mock(static_cast<int (overloaded_interface::*)(const std::string&)>(&overloaded_interface::foo)).when("hello").thenReturn(10);
+   mock(static_cast<int (overloaded_interface::*)(const std::string&)>(&overloaded_interface::foo)).when("bar").thenReturn(50);
+   mock(static_cast<int (overloaded_interface::*)(const std::string&)>(&overloaded_interface::foo)).when("hello").thenReturn(10);
    overloaded_interface& obj = mock.getInstance();
 
    ASSERT_EQ(0, obj.foo(true));
@@ -29,12 +27,12 @@ TEST(test_overloaded_functions, foo)
    ASSERT_EQ(0, obj.foo('A'));
    ASSERT_EQ(1, obj.foo('%'));
    ASSERT_THROW(obj.foo('#'), mockitopp::partial_implementation_exception);
-//   ASSERT_EQ(50, obj.foo("bar"));
-//   ASSERT_EQ(10, obj.foo("hello"));
-//   ASSERT_THROW(obj.foo("!@#$%"), mockitopp::partial_implementation_exception);
+   // XXX: explicitly specify std::string because of implicit boolean conversion (i.e. ISO 14882 sec 4.12)
+   ASSERT_EQ(50, obj.foo(std::string("bar")));
+   ASSERT_EQ(10, obj.foo(std::string("hello")));
+   ASSERT_THROW(obj.foo(std::string("!@#$%")), mockitopp::partial_implementation_exception);
 
    ASSERT_TRUE(mock(static_cast<int (overloaded_interface::*)(bool)>(&overloaded_interface::foo)).exactly(2));
    ASSERT_TRUE(mock(static_cast<int (overloaded_interface::*)(char)>(&overloaded_interface::foo)).exactly(3));
-//   ASSERT_TRUE(mock(static_cast<int (overloaded_interface::*)(std::string)>(&overloaded_interface::foo)).exactly(3));
+   ASSERT_TRUE(mock(static_cast<int (overloaded_interface::*)(const std::string&)>(&overloaded_interface::foo)).exactly(3));
 }
-
