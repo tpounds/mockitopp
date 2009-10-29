35d15fe806b080b52986233c374f9154367cf8fa
Trevor Pounds <trevor.pounds@gmail.com>
2009-10-29 22:01:05 +0000

    - add missing test case for memeber function pointer const/const volatile qualifier support

---
+++ test/src/test_mfp_cv.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+#include <mockitopp/MockObject.hpp>
+
+using mockitopp::MockObject;
+
+struct cv_interface
+{
+   virtual int __no_cv();
+   virtual int __has_c() const;
+   virtual int __has_cv() const volatile;
+};
+
+TEST(test_mfp_cv, compile)
+{
+   MockObject<cv_interface> mock;
+   mock(&cv_interface::__no_cv).when().thenReturn(0);
+   mock(&cv_interface::__has_c).when().thenReturn(0);
+   mock(&cv_interface::__has_cv).when().thenReturn(0);
+   cv_interface& obj = mock.getInstance();
+
+   ASSERT_EQ(0, obj.__no_cv());
+   ASSERT_EQ(0, obj.__has_c());
+   ASSERT_EQ(0, obj.__has_cv());
+}
