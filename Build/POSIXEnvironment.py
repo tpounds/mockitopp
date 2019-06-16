f78ffe7c2eb731a7ed64cc2ebec4d800808d27e5
Trevor Pounds <trevor.pounds@gmail.com>
2019-06-16 13:10:24 -0400

    Enable extra compiler warnings.

--- Build/POSIXEnvironment.py
+++ Build/POSIXEnvironment.py
@@ -11,4 +11,4 @@ class Environment(BaseEnvironment.Environment):
       cxx = os.path.basename(self['CXX'])
 
       if cxx.startswith("clang") or cxx.startswith("g++"):
-         self.AppendUnique(CXXFLAGS=['-Wall', '-pedantic', '-g'])
+         self.AppendUnique(CXXFLAGS=['-Wall', '-Wextra', '-Wpedantic', '-g'])
